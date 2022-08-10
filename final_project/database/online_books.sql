05514851dac8ba9e3efad62fe564b06f7eb69e16
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-08-11 02:23:51 +0600

    database added

--- final_project/database/online_books.sql
+++ final_project/database/online_books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2022 at 08:04 PM
+-- Generation Time: Aug 10, 2022 at 10:15 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `customer` (
   `id` int(128) NOT NULL,
-  `name` varchar(225) NOT NULL,
-  `email` varchar(225) NOT NULL,
-  `phone` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(125) NOT NULL,
   `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,7 +40,53 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `name`, `email`, `phone`, `address`) VALUES
-(1, 'Alamin Sheikh', 'alamin@gmail.com', '0129865325', 'dhaka');
+(1, 'Alamin Sheikh', 'alamin1@gmail.com', '01902954222', 'Dhaka ,Bangladesh'),
+(3, 'Alamin Sheikh', 'alamin@gmail.com', '01902954222', 'Dhaka ,Bangladesh');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `price` int(255) NOT NULL,
+  `image_text` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `image`, `title`, `price`, `image_text`) VALUES
+(1, 'Screenshot (57).png', 'demo product', 102, 'this is demo product');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reseller`
+--
+
+CREATE TABLE `reseller` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reseller`
+--
+
+INSERT INTO `reseller` (`id`, `name`, `email`, `phone`, `company`, `address`, `password`) VALUES
+(1, 'nafiz1', 'nafiz@gmail.com', '6546545646', 'demo', 'dhaka, bangladesh', '123456'),
+(2, 'Alamin Sheikh', 'alaminsheikh9745@gmail.com', '+8801902954222', 'alamin', 'Dhaka ,Bangladesh', '');
 
 -- --------------------------------------------------------
 
@@ -60,8 +106,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(7, 'admin', 'admin@gmail.com', 'admin1234'),
-(8, 'alamin', 'alamin@gmail.com', 'alamin1234');
+(8, 'alamin', 'alamin@gmail.com', 'alamin1234'),
+(17, 'admin', 'admin@gmail.com', 'admin1234');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +118,20 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 --
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reseller`
+--
+ALTER TABLE `reseller`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `users`
@@ -89,13 +148,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `reseller`
+--
+ALTER TABLE `reseller`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
